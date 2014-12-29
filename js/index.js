a6e7b610803372df0d289c0a4eddb70d0371f054
Dachi Gogotchuri <dachi.gogotchuri@gmail.com>
2014-12-30 00:55:10 +0100

    Visual Studio Web (HTML+JS+CSS) project template.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+﻿/* index.js */
+$(document).ready(function () {
+    "use strict";
+
+    var helloWorld = "Hello! You can use jQuery and more on Visual Studio too!";
+    var note = $("#note");
+    note.text(helloWorld);
+});
